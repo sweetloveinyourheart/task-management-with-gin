da5f564190879248d791075aeca371c395344cb2
nguyenlam <sweetloveinyourheart@gmail.com>
2023-12-31 10:08:02 +0700

    Init project

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	addr := fmt.Sprintf(":%s", port)
+	if err := r.Run(addr); err != nil {
+		log.Fatal("Error starting the server: ", err)
+	}
+}
