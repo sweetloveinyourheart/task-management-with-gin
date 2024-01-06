74553a0aee292b916cd84f9797e9d0ace6fa9890
nguyenlam <sweetloveinyourheart@gmail.com>
2024-01-06 23:08:16 +0700

    Update GORM schema

--- models/card.go
+++ models/card.go
@@ -1,17 +1,17 @@
 package models
 
 import (
-	"time"
+	"database/sql"
 
 	"gorm.io/gorm"
 )
 
 type Card struct {
 	gorm.Model
-	Title       string `gorm:"type:varchar(255);not null"`
-	Description string `gorm:"type:text"`
-	Position    *int   `gorm:"type:int"`
-	DueDate     *time.Time
+	Title       string        `gorm:"type:varchar(255);not null"`
+	Description string        `gorm:"type:text"`
+	Position    sql.NullInt64 `gorm:"type:int"`
+	DueDate     sql.NullTime
 	ListId      uint
 	List        List `gorm:"foreignKey:ListId"`
 	Comments    []Comment
--- models/list.go
+++ models/list.go
@@ -1,11 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
 
 type List struct {
 	gorm.Model
 	Title    string `gorm:"type:varchar(255);not null"`
 	BoardId  uint
-	Board    Board `gorm:"foreignKey:BoardId"`
-	Position *int  `gorm:"type:int"`
+	Board    Board         `gorm:"foreignKey:BoardId"`
+	Position sql.NullInt64 `gorm:"type:int"`
 }
